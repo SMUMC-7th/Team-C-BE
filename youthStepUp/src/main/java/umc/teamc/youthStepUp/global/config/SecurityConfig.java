a184e5e24ae38e8347aafbb28b03c25b1d282b2c
chaerish <ccyy8432@naver.com>
2024-10-31 02:05:38 +0900

    feat(SecurityConfig): SecurityConfig 설정
    스프링 시큐리티 설정 파일을 만들고, 관련된 설정을 한다.
    
    resolve: #4

---
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/global/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package umc.teamc.youthStepUp.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(new CorsConfig().corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/swagger-ui/index.html").permitAll()
+                        .anyRequest().permitAll()
+                );
+
+
+        return http.build();
+
+    }
+
+}
