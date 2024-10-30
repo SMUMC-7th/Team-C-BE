f0c91812c8d671be9e6602b3ef81caf969ebf441
chaerish <ccyy8432@naver.com>
2024-10-31 01:10:49 +0900

    feat: set up project

---
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/YouthStepUpApplication.java
@@ -0,0 +1,13 @@
+package umc.teamc.youthStepUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthStepUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthStepUpApplication.class, args);
+	}
+
+}
---
+++ youthStepUp/src/test/java/umc/teamc/youthStepUp/YouthStepUpApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.teamc.youthStepUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthStepUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
