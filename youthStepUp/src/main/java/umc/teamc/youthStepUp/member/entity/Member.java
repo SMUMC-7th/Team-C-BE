468cdee91a2f2afd57d19acbd504ac237f324f2e
chaerish <ccyy8432@naver.com>
2024-10-31 16:58:39 +0900

    feat(Member): Member 지역,전공,키워드 필드 추가

--- youthStepUp/src/main/java/umc/teamc/youthStepUp/member/entity/Member.java
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/member/entity/Member.java
@@ -1,5 +1,6 @@
 package umc.teamc.youthStepUp.member.entity;
 
+import jakarta.persistence.Access;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,8 +8,17 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,5 +30,14 @@ public class Member {
     @Enumerated(value= EnumType.STRING)
     @Column(name = "education")
     Education education;
+    @Enumerated(value= EnumType.STRING)
+    @Column(name = "region")
+    Region region;
+    @Enumerated(value= EnumType.STRING)
+    @Column(name = "category")
+    Major major;
+    @Enumerated(value= EnumType.STRING)
+    @Column(name = "keyword")
+    Keyword keyword;
 
 }
