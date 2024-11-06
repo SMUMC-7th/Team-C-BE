c7111fcd3a829f94211c6f2c8be91f075112852a
CHO <imalex12@naver.com>
2024-11-07 00:23:08 +0900

    merge (all): main설정 병합
     main설정 병합
    
    resolve: #7

--- youthStepUp/src/main/java/umc/teamc/youthStepUp/member/entity/Education.java
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/member/entity/Education.java
@@ -1,14 +1,21 @@
 package umc.teamc.youthStepUp.member.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
-
 public enum Education {
     HIGH_SCHOOL("고등학교 졸업 이하"),
     UNDERGRADUATE("대학교 재학"),
     COLLEGE_GRADUATE("대학교 졸업");
     final String description;
-    Education(String description){
-        this.description =description;
+
+    Education(String description) {
+        this.description = description;
+    }
+
+    public static Education toEducation(String description) {
+        for (Education education : Education.values()) {
+            if (education.description.equals(description)) {
+                return education;
+            }
+        }
+        return null;
     }
 }
