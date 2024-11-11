42a418790eff81664f51e81c2703965d5cf0c268
CHO <imalex12@naver.com>
2024-11-11 21:23:46 +0900

    refactor(main): @EnableJpaAuditing 추가

--- youthStepUp/src/main/java/umc/teamc/youthStepUp/YouthStepUpApplication.java
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/YouthStepUpApplication.java
@@ -2,12 +2,14 @@ package umc.teamc.youthStepUp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class YouthStepUpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(YouthStepUpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(YouthStepUpApplication.class, args);
+    }
 
 }
