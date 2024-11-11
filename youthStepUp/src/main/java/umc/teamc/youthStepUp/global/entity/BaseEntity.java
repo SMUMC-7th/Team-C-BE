3a9b48fc82a63f67199b0131c3a29a64f7de5492
CHO <imalex12@naver.com>
2024-11-11 21:20:55 +0900

    refactor(baseEntity): baseEntity에 삭제로직 추가

--- youthStepUp/src/main/java/umc/teamc/youthStepUp/global/entity/BaseEntity.java
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/global/entity/BaseEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -29,4 +28,8 @@ public abstract class BaseEntity {
 
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
+
+    public void deleteEntity() {
+        this.deletedAt = LocalDateTime.now();
+    }
 }
