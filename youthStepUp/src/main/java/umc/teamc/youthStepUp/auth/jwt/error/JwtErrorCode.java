e421be6cb02d67372df8343b796f0c0dff5511c2
chaerish <ccyy8432@naver.com>
2024-11-12 22:10:57 +0900

    feat: jwt filter 비회원 통과시키는 로직 추가 및 에러코드 추가

--- youthStepUp/src/main/java/umc/teamc/youthStepUp/auth/jwt/error/JwtErrorCode.java
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/auth/jwt/error/JwtErrorCode.java
@@ -8,10 +8,11 @@ import umc.teamc.youthStepUp.global.error.BaseErrorCode;
 @AllArgsConstructor
 @Getter
 public enum JwtErrorCode implements BaseErrorCode {
-    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "TOKEN401", "토큰이 만료되었습니다."),
-    TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "TOKEN401", "유효한 토큰 형식이 아닙니다."),
-    HEADER_NO_TOKEN(HttpStatus.BAD_REQUEST, "TOKEN400", "헤더가 비어있습니다."),
-    TOKEN_NO_AUTH(HttpStatus.UNAUTHORIZED, "TOKEN401", "인증자격이 없습니다.");
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "TOKEN4001", "토큰이 만료되었습니다."),
+    TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "TOKEN4002", "유효한 토큰 형식이 아닙니다."),
+    HEADER_NO_TOKEN(HttpStatus.BAD_REQUEST, "TOKEN4003", "헤더가 비어있습니다."),
+    TOKEN_NO_AUTH(HttpStatus.UNAUTHORIZED, "TOKEN4004", "인증자격이 없습니다."),
+    REFRESH_TOKEN_MISSING(HttpStatus.BAD_REQUEST, "TOKEN4005", "쿠키에 리프레쉬 토큰이 존재하지 않습니다.");
     private final HttpStatus status;
     private final String code;
     private final String message;
