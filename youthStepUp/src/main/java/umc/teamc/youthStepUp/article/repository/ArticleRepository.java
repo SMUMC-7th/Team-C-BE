54578ffb336a90ae7e4bdf4f12de144ddd10b2d0
RCNR <iop04226@gmail.com>
2024-11-21 19:27:43 +0900

    refactor(ArticleRepository): NULL인 경우만 조회
    member의 deleted_at이 NULL인 경우만 조회

--- youthStepUp/src/main/java/umc/teamc/youthStepUp/article/repository/ArticleRepository.java
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/article/repository/ArticleRepository.java
@@ -3,9 +3,12 @@ package umc.teamc.youthStepUp.article.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import umc.teamc.youthStepUp.article.entity.Article;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    @Query("SELECT a FROM Article a WHERE a.deletedAt IS NULL AND a.member.deletedAt IS NULL")
     Slice<Article> findByIdLessThanOrderByIdDesc(Long cursorId, Pageable pageable);
+    
 }
