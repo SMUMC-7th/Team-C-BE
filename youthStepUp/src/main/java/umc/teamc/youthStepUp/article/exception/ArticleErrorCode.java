9799833892248b50c4d07358c7b5901e48dd764e
RCNR <iop04226@gmail.com>
2024-11-21 19:28:28 +0900

    feat(ArticleErrorCode): 에러 코드 추가
    접근 권한 없는 에러 코드 추가

--- youthStepUp/src/main/java/umc/teamc/youthStepUp/article/exception/ArticleErrorCode.java
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/article/exception/ArticleErrorCode.java
@@ -9,7 +9,8 @@ import umc.teamc.youthStepUp.global.error.BaseErrorCode;
 @Getter
 public enum ArticleErrorCode implements BaseErrorCode {
 
-    NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE404", "게시글을 찾지 못했습니다."),;
+    NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE404", "게시글을 찾지 못했습니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "ARTICLE403", "접근 권한이 없습니다.");
 
     private final HttpStatus status;
     private final String code;
