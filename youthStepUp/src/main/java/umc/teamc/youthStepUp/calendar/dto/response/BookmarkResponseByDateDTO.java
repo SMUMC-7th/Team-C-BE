eee60a89d1a48c4431b7be76551874e1ba19da3b
chaerish <ccyy8432@naver.com>
2024-12-02 12:44:03 +0900

    bug: 캘린더 월,일 기준 검색 오류 수정

--- youthStepUp/src/main/java/umc/teamc/youthStepUp/calendar/dto/response/BookmarkResponseByDateDTO.java
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/calendar/dto/response/BookmarkResponseByDateDTO.java
@@ -2,16 +2,15 @@ package umc.teamc.youthStepUp.calendar.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-
 import java.time.LocalDate;
+import lombok.Builder;
 
 @Builder
 public record BookmarkResponseByDateDTO(
         @NotNull Long id,
         @NotNull String name,
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDate startDate,
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDate endDate,
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate endDate,
         @NotNull boolean isCompleted,
         @NotNull String srchPolicyId
 ) {
--- youthStepUp/src/main/java/umc/teamc/youthStepUp/calendar/dto/response/BookmarkResponseByMonthDTO.java
+++ youthStepUp/src/main/java/umc/teamc/youthStepUp/calendar/dto/response/BookmarkResponseByMonthDTO.java
@@ -2,15 +2,14 @@ package umc.teamc.youthStepUp.calendar.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
-
 import java.time.LocalDate;
+import lombok.Builder;
 
 @Builder
 public record BookmarkResponseByMonthDTO(
         @NotNull Long id,
         @NotNull String name,
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDate startDate,
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDate endDate
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate startDate,
+        @JsonFormat(pattern = "yyyy-MM-dd") LocalDate endDate
 ) {
 }
